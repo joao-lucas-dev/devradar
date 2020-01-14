4e1665ab94597e18529ec1d0c1180886e18d48e5
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-14 17:26:56 -0300

    feat: create devs

---
+++ backend/src/app/controllers/DevController.js
@@ -0,0 +1,54 @@
+import Dev from '../models/Dev';
+import * as Yup from 'yup';
+
+import api from '../../services';
+
+class DevController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      github_username: Yup.string().required(),
+      techs: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(404).json({ error: 'Validation fails' });
+    }
+
+    const { github_username, techs, longitude, latitude } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (dev) {
+      return res.status(404).json({ error: 'User already registered' });
+    }
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const response = await api.get(`/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = response.data;
+
+    const location = {
+      type: 'Point',
+      coordinates: [
+        longitude,
+        latitude
+      ]
+    }
+
+    dev = await Dev.create({
+      github_username,
+      name,
+      avatar_url,
+      bio,
+      techs: techsArray,
+      location,
+    });
+
+    return res.json(dev);
+  }
+}
+
+export default new DevController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import DevController from './app/controllers/DevController';
+
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ ok: true }));
+routes.post('/devs', DevController.store);
 
 export default routes;
