4d9ccb393a9132d97900e5a087a3fc755ccc63f5
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-15 17:45:14 -0300

    feat: sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+
 function App() {
-  return <h1>Hello</h1>
+  return(
+    <div className="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" type="text" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" type="text" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="">Latitude</label>
+              <input name="latitude" type="text" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="">Longitude</label>
+              <input name="longitude" type="text" id="Longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main></main>
+    </div>
+  )
 }
 
 export default App;
